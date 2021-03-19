8e076c307fb138f586f4bea24880c1d85c43934e
Azim-Ahmed <azimuahmed13@gmail.com>
2021-03-19 17:00:03 +0600

    aurora effect added

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,13 +6,20 @@ import styles from './Home.module.scss';
 
 export default function Home() {
   return (
-    <AppLayout pageTitle='Markopolo.ai' description='Markopolo.ai'>
+    <AppLayout pageTitle="Aurora UI" description="Aurora UI">
       <div className={styles.container}>
         <main className={styles.main}>
-          <img src='/kabiiir23.svg' alt='kabiiir23' />
+          {/* <img src='/kabiiir23.svg' alt='kabiiir23' />
           <ButtonPrimary onClick={() => {}}>
         Here
-          </ButtonPrimary>
+          </ButtonPrimary> */}
+          <div className={styles.wrapper}>
+
+          
+            <div className={styles.base && styles.one}></div>
+            <div className={styles.base && styles.two}></div>
+            <div className={styles.base && styles.three}></div>
+          </div>
         </main>
       </div>
     </AppLayout>
